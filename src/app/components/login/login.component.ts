8396bec27efd9ba90c0998a9dbeec5caae83e309
Tonny James P. dos Reis <tonny.jreis@outlook.com>
2021-12-19 14:44:07 -0300

    authenticação para a pagina login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -18,22 +20,24 @@ export class LoginComponent implements OnInit {
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
 
-  constructor(private toast: ToastrService) { }
+  constructor(
+    private toast: ToastrService,
+    private service: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  logar(){
-     this.toast.error('Usuario e/ou Senha inválidos', 'Login');
-     this.creds.senha = '';
+  logar() {
+    this.service.authenticate(this.creds).subscribe(resposta => {
+      this.service.successFulLogin(resposta.headers.get('Authorization').substring(7));
+      this.router.navigate([''])
+    },
+      () => {
+        this.toast.error('Usuario e/ou senha inválidos');
+      })
   }
-  validaCampos(): boolean{
-    if(this.email.valid && this.senha.valid){
-      return true;
-    }else {
-      return false;
-    }
+  validaCampos(): boolean {
+    return this.email.valid && this.senha.valid
   }
-
-
 }
