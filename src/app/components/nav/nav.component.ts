6be0218caec1e163c17ed86f3f42c59b1cd2c5b9
Tonny James P. dos Reis <tonny.jreis@outlook.com>
2021-12-21 15:04:50 -0300

    html css para componente create

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -15,7 +15,7 @@ export class NavComponent implements OnInit {
     private toast: ToastrService) { }
 
   ngOnInit(): void {
-    this.router.navigate(['tecnicos'])
+    this.router.navigate(['tecnicos/create'])
   }
 
   logout() {
