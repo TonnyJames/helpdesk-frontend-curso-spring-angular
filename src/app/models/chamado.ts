3b4f568c2ff6fc1eb95829e4281390ebadce1729
Tonny James P. dos Reis <tonny.jreis@outlook.com>
2021-12-22 14:36:55 -0300

    inteface para chamados criada

---
+++ src/app/models/chamado.ts
@@ -0,0 +1,12 @@
+export interface Chamado{
+    id?: any;
+    dataAbertura?: string;
+    dataFechamento?: string;
+    prioridade: string;
+    status: string;
+    titulo: string;
+    tecnico: any;
+    cliente: any;
+    nomeCliente: string;
+    nomeTecnico: string;
+}
